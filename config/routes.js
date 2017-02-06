45a5b79224c299798a099cf2a0f7f810ffb1bf5d
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2017-02-06 18:34:47 +0200

    authentication edit

--- config/routes.js
+++ config/routes.js
@@ -29,29 +29,29 @@ router.post("/user",
   validate.validateBody("user", "save"),
   userCtrl.saveOne);
 
-router.use("", auth.authenticate);
+//router.use("", auth.authenticate);
 
 // Routes for all users
 
-router.put("/user/:id", userCtrl.updateOne);
-router.post("/room", roomCtrl.saveOne);
-router.post("/room/:id/message", msgCtrl.saveOne);
-router.post("/room/:id", roomCtrl.sendRoomPassword);
-router.put("/room/:id", roomCtrl.editRoom);
-router.get("/room/:id/member", roomCtrl.findMembers);
-router.post("/room/:id/member", roomCtrl.setMember);
-router.get("/user/:name", userCtrl.findByName);
-router.put("/user/:id/block", userCtrl.blockUser);
-router.get("/blockedusers", userCtrl.blockedUsers);
-router.put("/user/:id/unblock", userCtrl.unblockUser);
-router.put("/user", userCtrl.editProfile);
+router.put("/user/:id", auth.authenticate, userCtrl.updateOne);
+router.post("/room", auth.authenticate, roomCtrl.saveOne);
+router.post("/room/:id/message", auth.authenticate, msgCtrl.saveOne);
+router.post("/room/:id", auth.authenticate, roomCtrl.sendRoomPassword);
+router.put("/room/:id", auth.authenticate, roomCtrl.editRoom);
+router.get("/room/:id/member", auth.authenticate, roomCtrl.findMembers);
+router.post("/room/:id/member", auth.authenticate, roomCtrl.setMember);
+router.get("/user/:name", auth.authenticate, userCtrl.findByName);
+router.put("/user/:id/block", auth.authenticate, userCtrl.blockUser);
+router.get("/blockedusers", auth.authenticate, userCtrl.blockedUsers);
+router.put("/user/:id/unblock", auth.authenticate, userCtrl.unblockUser);
+router.put("/user", auth.authenticate, userCtrl.editProfile);
 
 // Routes accessable only for admin
 
-router.use("", auth.onlyAdmin);
+//router.use("", auth.onlyAdmin);
 
-router.get("/user", userCtrl.findAll);
-router.delete("/user/:id", userCtrl.deleteOne);
+//router.get("/user", userCtrl.findAll);
+//router.delete("/user/:id", userCtrl.deleteOne);
 
 router.use("", errorHandler.handleErrors);
 
